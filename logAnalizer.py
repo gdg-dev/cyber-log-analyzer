07f2f1c4a2c93eb51a237b5fab435d4f627b6f08
Gabriel <gabrielgomes7410@gmail.com>
2026-08-31 16:32:23 +0100

    started inheritence this is not for the code i wrote was just to test how it works

--- logAnalizer.py
+++ logAnalizer.py
@@ -1,7 +1,7 @@
 
 #class stuctur
 class LogEntery:
-    
+    language = ["English"]
     def __init__(self, ip_adress, status, timestamp):
         self.ip_adress = ip_adress
         self.status = status
@@ -9,8 +9,31 @@ class LogEntery:
 
 
     def __str__(self):
-        return f"{self.ip_adress}, {self.status} {self.timestamp}"
+        return f"{self.language}, {self.ip_adress}, {self.status} {self.timestamp}"
 
+
+
+class Information(LogEntery):
+
+
+    def __init__(self):
+        super().__init__(self.language)
+        self.language = self.language + ["German"]
+
+    def __str__(self):
+        return f"languages {self.language}"
+
+
+
+
+list = []
 
 a = LogEntery("1.1.1", "online", "28/8/2026")
-print(a)
+b = LogEntery("1.1.2", "ofline", "21/8/2026")
+c = Information()
+list.append(a)
+list.append(b)
+print(c)
+
+for object in list:
+    print(object)
