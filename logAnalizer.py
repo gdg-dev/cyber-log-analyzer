217fbe80b26ebce371c22b3e6b5a58569ba6a216
Gabriel <gabrielgomes7410@gmail.com>
2026-09-03 15:51:45 +0100

    just working to understand how super() workd

--- logAnalizer.py
+++ logAnalizer.py
@@ -1,4 +1,4 @@
-
+"""
 #class stuctur
 class LogEntery:
     language = ["English"]
@@ -17,7 +17,7 @@ class Information(LogEntery):
 
 
     def __init__(self):
-        super().__init__(self.language)
+        super().__init__()
         self.language = self.language + ["German"]
 
     def __str__(self):
@@ -30,10 +30,67 @@ list = []
 
 a = LogEntery("1.1.1", "online", "28/8/2026")
 b = LogEntery("1.1.2", "ofline", "21/8/2026")
-c = Information()
+
 list.append(a)
 list.append(b)
-print(c)
+
 
 for object in list:
     print(object)
+"""
+
+
+
+
+class Dog:
+
+    specie = "Canis familiaris"
+
+
+    def __init__(self, name, age, breed):
+        self.name = name 
+        self.age = age
+        self.breed  = breed
+
+
+
+    def __str__(self):
+        return f"{self.name} {self.age} {self.breed}"
+
+
+    def speak(self, sound):
+        return f"{self.name} Bark {sound}"
+
+
+class Bulldog(Dog):
+
+
+    def speak(self, sound="wof"):
+        return super().speak(sound)
+
+class Dachshund(Dog):
+
+    def speak(self, sound="rof"):
+        return super().speak(sound)
+
+    
+class RussellTerrier(Dog):
+
+   def speak(self, sound="miau"):
+       return super().speak(sound)
+
+
+budy = RussellTerrier("budy", 12, "dog")
+print(budy.speak())
+
+
+
+
+
+
+
+
+
+
+
+
