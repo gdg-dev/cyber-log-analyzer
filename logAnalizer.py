c7ed3c53c930e64f14fabc0a68f3b7f8de7dda31
Gabriel <gabrielgomes7410@gmail.com>
2026-09-04 15:53:47 +0100

    Working with classes

--- logAnalizer.py
+++ logAnalizer.py
@@ -41,7 +41,7 @@ for object in list:
 
 
 
-
+"""
 class Dog:
 
     specie = "Canis familiaris"
@@ -79,18 +79,47 @@ class RussellTerrier(Dog):
    def speak(self, sound="miau"):
        return super().speak(sound)
 
-
-budy = RussellTerrier("budy", 12, "dog")
-print(budy.speak())
+"""
 
 
-
+class User:
 
 
 
+    passwords = [{"name": "Admin", "pass": "123"}]
 
 
+    def __init__(self, name, age, ip_adress):
+        self.name = name
+        self.age = age
+        self.ip_adress = ip_adress
 
 
+    def __str__(self):
+        return f"\n-Name: {self.name};\n-Age: {self.age};\n-IP: {self.ip_adress}"
+
+
+    def create_password(self, password):
+        self.passwords.append({"name": self.name, "pass": password})
+        return self.passwords
+
+
+    def log_in(self, password_given):
+        if self.passwords[self.name] == password_given:
+            return True
+        else:
+            return False
+
+juan = User("juan", 42, "1.1.1.1")
+print(juan)
+juan.create_password("abc")
+
+paulo = User("Paulo", 47, "2.2.2.2")
+print(paulo)
+print(paulo.create_password("my_pass"))
+
+
+print(paulo.log_in("my_pass"))
+print(paulo.log_in("123"))
 
 
